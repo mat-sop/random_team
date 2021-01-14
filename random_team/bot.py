458fc377588d37df5360d337c24ed3f9e0d3dd5c
Mateusz Sopiński <mateuszsopinski243@gmail.com>
2021-01-14 21:22:24 +0100

    Add python-dotenv

--- random_team/bot.py
+++ random_team/bot.py
@@ -3,11 +3,15 @@ import os
 from discord.ext import commands
 
 from cogs.random import Random
+from dotenv import load_dotenv
+
+
+load_dotenv()
+token = os.getenv('DISCORD_BOT_TOKEN')
 
 bot_description = 'Bot that splits members into teams.'
 bot = commands.Bot(command_prefix='$rng ', description=bot_description)
 
 bot.add_cog(Random(bot))
 
-token = os.getenv('DISCORD_BOT_TOKEN', '')
 bot.run(token)
